920ce790a9b96cf18dba4893170947c9b84235e8
Saussereau Dominique <domsau2@yahoo.fr>
2020-05-09 12:40:05 +0200

    Minor.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-void InitRandMatrix1D(vector<int>& MatrixInit1D, int minData = 0, int maxData = 256) {
+void initRandMatrix1D(vector<int>& MatrixInit1D, int minData = 0, int maxData = 256) {
     for(int a = 1; a < MatrixInit1D.size() + 1; a++) {
        if ( maxData < minData ) { // Usually, the minimum is before the maximum, and sometimes it is not the default value.
           auto temp = maxData;
@@ -13,7 +13,7 @@ void InitRandMatrix1D(vector<int>& MatrixInit1D, int minData = 0, int maxData = 
         }
     }
 
-    void CoutMatrix1D(vector<int>& CoutMatrix1D) {
+void coutMatrix1D(vector<int>& CoutMatrix1D) {
     for(int a = 1; a < CoutMatrix1D.size() + 1; a++) {
       cout << CoutMatrix1D[a] << endl;
         }
@@ -22,8 +22,8 @@ void InitRandMatrix1D(vector<int>& MatrixInit1D, int minData = 0, int maxData = 
 int main() {
     int N = 42;
     vector<int> Matrix(N, 0);
-    InitRandMatrix1D(Matrix, 0, 42);
-    CoutMatrix1D(Matrix);
+    initRandMatrix1D(Matrix, 0, 42);
+    coutMatrix1D(Matrix);
     cout << "Ok!" << endl;
     return 0;
     }
